02a4acb2fbc44c4848882f32d61f655287d93e4a
RunningLiLi <3238367573@qq.com>
2023-05-03 09:07:59 +0800

    fix(app.tsx): 大小写问题

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom/client";
 import React from "react";
-import App from "./App.tsx";
+import App from "./app.tsx";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
